4e32e1dad899363cc145fb2a2ffc0f0860ae4752
Calvinda <calvinda@fazzo-creative.com>
2019-03-04 14:01:08 +0800

    gulp rebuild

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('autoprefixer');
-var postcss = require('gulp-postcss');
-var customMedia = require("postcss-custom-media")
-var lost = require('lost')
-var rucksackCss = require('rucksack-css')
 
 var paths = {
   cssSource: 'sass/',
-  cssOutput: 'css/'
-}
-gulp.watch(paths.cssSource + '**/*.scss', ['sass']);
+  cssOutput: 'css/',
+};
 
-gulp.task('sass', function () {
-  var processors = [                                
-      autoprefixer({browsers: ['last 5 version']}),
-      customMedia(),
-      lost(),
-      rucksackCss()   
-  ];
+gulp.task('watch', function() {
+  gulp.watch(paths.cssSource + '**/*.scss', gulp.series('sass'));
+});
 
-  return gulp.src('./sass/**/*.scss')
+gulp.task('sass', function() {
+  var processors = [autoprefixer({ browsers: ['last 5 version'] })];
+
+  return gulp
+    .src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(postcss(processors))
+
     .pipe(gulp.dest('./css'));
 });
 
-
-
-gulp.task('default', ['sass']);
+gulp.task('default', gulp.parallel('watch', 'sass'));
